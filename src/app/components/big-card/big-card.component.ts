f56e0fd28637d1f02cf93a8e6846a83bf8c07752
DiogoLeonardoo <diogoleo57@gmail.com>
2023-05-24 16:03:20 -0300

    Dinamicidade no Angular

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+
+export class BigCardComponent  { 
+  
+  photoCover: string = "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhOfM8JE_Bh4oxeJWqFiH-p9yd5DBgsrV6G03E7AvyVJDCidwWykrm-0hL-oN4-rSBSgs95hv14Q4wHxwqe0a3e4_n-L7PlFmfAOcWM-9Iv645c74ggK6K8glPD5JvtrsBDOQ68-9T6TaiHJXssPS4Ugsc09jAyBbQAJofnwaeNhx0ev2u1wQ/s540/movies.jpg"
+  cardTitle: string = "Marvel anuncia nova geração"
+  cardDescription: string = "A marvel anunciou nova geração na suas histórias e séries da plataforma de streaming Disney +"
+  
 
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent {
+    photoCover: string = "https://sm.ign.com/ign_br/screenshot/default/img-worlds-of-adventure-11-1-825x464_sa4d.jpg"
+    cardTitle: string = "Novo Seriado no STAR+ e Disney +"
 
 }
